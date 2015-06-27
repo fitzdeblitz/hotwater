a5378514a77e770416c9cf1d2eb5f9173a6d8531
fitzdeblitz <fitzdeblitz@hotmail.com>
2015-06-28 00:07:23 +0100

    corrected temp sensor names

--- sensormgr.py
+++ sensormgr.py
@@ -6,8 +6,8 @@ except ImportError:
 
 class SensorMgr:
     def __init__(self):
-        self._roof_green_house = TempSensor('Gable Solar Panel', '28-000005e4b7ba')
-        self._gable = TempSensor('Roof Green House', '28-000005e77594')
+        self._roof_green_house = TempSensor('Roof Green House', '28-000005e4b7ba')
+        self._gable = TempSensor('Gable Solar Panel', '28-000005e77594')
         self._tank_lower = TempSensor('Tank Lower', '28-0414608267ff')
         self._tank_upper = TempSensor('Tank Upper', '28-041460ba02ff')
         self._back_boiler = TempSensor('Back Boiler', '28-0414609848ff')

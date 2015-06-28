51c5a211707116c2891b51235a04f1a70bdcfe07
fitzdeblitz <fitzdeblitz@hotmail.com>
2015-06-28 11:39:25 +0100

    added logging when turning gable pump on/off

--- controller.py
+++ controller.py
@@ -18,8 +18,10 @@ class ControllerThread(Thread):
     def run(self):
         while True:
             if self.sensormgr.is_gable_hotter_than_tank():
+                logging.debug('turning gable pump on')
                 self.switchmgr.turn_gable_pump_on()
             else:
+                logging.debug('turning gable pump off')
                 self.switchmgr.turn_gable_pump_off()
 
             time.sleep(5)

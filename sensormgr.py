4d8f06f8256248524d7049e25c72ae57b3db9d89
fitzdeblitz <fitzdeblitz@hotmail.com>
2015-06-28 11:21:15 +0100

    added hacky temp offset thingy

--- sensormgr.py
+++ sensormgr.py
@@ -6,6 +6,9 @@ except ImportError:
 
 class SensorMgr:
     def __init__(self):
+        # offset to avoid switches being turned on/off too often
+        self._temp_offset = 2
+
         self._roof_green_house = TempSensor('Roof Green House', '28-000005e4b7ba')
         self._gable = TempSensor('Gable Solar Panel', '28-000005e77594')
         self._tank_upper = TempSensor('Tank Upper', '28-041460ba02ff')
@@ -21,10 +24,10 @@ class SensorMgr:
         ]
 
     def is_gable_hotter_than_tank(self):
-        return self._gable.read_temp() > self._tank_lower.read_temp()
+        return self._gable.read_temp() > self._tank_lower.read_temp() + self._temp_offset
 
     def is_back_boiler_hotter_than_tank(self):
-        return self._back_boiler.read_temp() > self._tank_upper.read_temp()
+        return self._back_boiler.read_temp() > self._tank_upper.read_temp() + self._temp_offset
 
     def get_tempsensors(self):
         result = []
